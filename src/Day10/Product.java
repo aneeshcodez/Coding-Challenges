338ea2ec67c3797a1934c5fa4db3eba3508cbbc1
Aneeshsshankar <aneeshsshankar@gmail.com>
2026-03-09 23:46:50 +0530

    Day 10

---
+++ src/Day10/Product.java
@@ -0,0 +1,54 @@
+package Day10;
+
+import java.util.List;
+
+public class Product {
+    Long id;
+    String name;
+    String category;
+    double price;
+
+    public Product(long l, String laptop, String electronics, int i) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    List<Product> products = List.of(
+            new Product(1L, "iPhone", "Electronics", 90000),
+            new Product(2L, "Laptop", "Electronics", 70000),
+            new Product(3L, "Keyboard", "Electronics", 1500),
+            new Product(4L, "Shoes", "Fashion", 3000),
+            new Product(5L, "T-Shirt", "Fashion", 800),
+            new Product(6L, "Rice Bag", "Grocery", 1200)
+    );
+}
---
+++ src/Day10/ProductDto.java
@@ -0,0 +1,6 @@
+package Day10;
+
+public class ProductDto {
+    String name;
+    double price;
+}
