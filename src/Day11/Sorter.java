1d4fee91f66fe6e2f4a47d71d9b5b098682159b6
Aneeshsshankar <aneeshsshankar@gmail.com>
2026-03-14 14:50:43 +0530

    Day 11

--- src/Day11/Sorter.java
+++ src/Day11/Sorter.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Sorter {
     public static List<String> sortByName(List<Product> products){
-        List<String> nameSortedList = products.stream().map(product -> product.getName())
+        List<String> nameSortedList = products.stream().map(Product::getName)
                                       .sorted().toList();
 
 
@@ -14,7 +14,10 @@ public class Sorter {
     }
 }
 
-// Learn method references
 
 // The Java Stream map() method is used to return a new stream of objects
-// .map(product -> product.getName()) =
+// .map(product -> product.getName()) = .map(Product::getName)
+
+// Learn 3 in blue book
+// 3-4 challenges
+// prod code
