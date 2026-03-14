340f067bdf12475a466c508bbe17f69c1b398ee2
Aneeshsshankar <aneeshsshankar@gmail.com>
2026-03-14 10:44:59 +0530

    Day 11

--- src/Day11/Sorter.java
+++ src/Day11/Sorter.java
@@ -6,22 +6,15 @@ import java.util.stream.Collectors;
 
 public class Sorter {
     public static List<String> sortByName(List<Product> products){
-        List<String> listOfNames = new ArrayList<>();
-        for(Product product : products){
-            String name = product.getName();
-            listOfNames.add(name);
-        }
-        List<String> priceSortedList = listOfNames.stream().sorted().collect(Collectors.toUnmodifiableList());
-        return priceSortedList;
+        List<String> nameSortedList = products.stream().map(product -> product.getName())
+                                      .sorted().toList();
+
+
+        return nameSortedList;
     }
 }
 
-// Aready started Day 11
-// I was trying to accomplish 1st task of Day 11 by following Alexander Obregon in Medium
-// Pls finish this task and all other task in that Day
-
-// Try to use .map() instead of looping so first learn it
-// Update in Notion
+// Learn method references
 
 // The Java Stream map() method is used to return a new stream of objects
-//
+// .map(product -> product.getName()) =
